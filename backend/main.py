c3d9ce36b0b92d7f0789c70e7637334c20141813
Ashwin Gururaj <ashg2099@outlook.com>
2026-06-26 13:05:23 +1000

    chore: add render.yaml + open CORS for deployment

--- backend/main.py
+++ backend/main.py
@@ -38,7 +38,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001", "https://*.vercel.app", "*",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
